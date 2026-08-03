226b6e23d9a807e917dc84f41b24e83d13a5f6bb
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-08-03 14:46:11 +0530

    Adding the brute force solution for 2 pointers for finding subarray and substrings with if loop optimization for fixing the window length

---
+++ sliding-window-2-pointers/Longest Subarray and Substring/brute-force.py
@@ -0,0 +1,18 @@
+"""Generate longest subarray/all subarray with <condition>"""
+#Generate all subarrays with sum <=k
+nums = [2,5,1,7,10]
+k = 14
+n = len(nums)
+left = 0
+right = 0
+total = 0
+maxlen = 0
+while right < n:
+    total += nums[right]
+
+    if total > k: #if they ask actual element to be printed use the while > k condition
+        total -= nums[left]
+        left+=1
+    maxlen = max(maxlen, right - left + 1)
+    right+=1
+print(maxlen)
