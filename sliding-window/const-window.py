411f5de8c835e827de75e076890fba22c40d59e5
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-08-03 11:30:34 +0530

    learning the concept of two pointers in sliding window for constant window

---
+++ sliding-window/const-window.py
@@ -0,0 +1,18 @@
+#Largest subarray of n consecutive elements with maxsum
+nums = [-1, 2, 3, 3, 4, 5, -1]
+k = 4
+n = len(nums)
+if n < k:
+    print(None)
+l, r = 0, k
+current_sum = 0
+maxm = float("-inf")
+for i in range(k):
+    current_sum+=nums[i]
+
+while r < n: # for r in range(k,n): and remove that r+=1 from below it will work with for loop too
+    current_sum = current_sum - (nums[l]) + nums[r]
+    l+=1
+    r+=1
+    maxm = max(maxm, current_sum)
+print(maxm)
